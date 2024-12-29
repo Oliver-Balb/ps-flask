9bcbd4021286432f8b5177615e6b7a9b0609a964
p312734 <34969190+p312734@users.noreply.github.com>
2024-12-29 21:37:14 +0100

    Looping

--- flashcards.py
+++ flashcards.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 from model import db
 
 app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return render_template("welcome.html", message="Here is a message from the view.")
+    return render_template("welcome.html", 
+                           cards=db
+                           )
 
-@app.route("/card")
-def card_view():
-    card = db[0]
-    return render_template("card.html", card=card)
+@app.route("/card/<int:index>")
+def card_view(index):
+    try:
+        card = db[index]
+        return render_template("card.html", card=card, index=index, max_index=len(db)-1)
+    except IndexError:
+        abort(404)
