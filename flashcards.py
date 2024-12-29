c8b963524f9b7cf03224ebb4f02c5cd71d63a153
p312734 <34969190+p312734@users.noreply.github.com>
2024-12-29 23:51:57 +0100

    add_card completed

--- flashcards.py
+++ flashcards.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, abort, jsonify
-from model import db
+from flask import Flask, render_template, abort, jsonify, request, redirect, url_for
+from model import db, save_db
 
 app = Flask(__name__)
 
@@ -16,6 +16,18 @@ def card_view(index):
         return render_template("card.html", card=card, index=index, max_index=len(db)-1)
     except IndexError:
         abort(404)
+        
+@app.route("/add_card", methods=["GET", "POST"])
+def add_card():
+    if request.method == "POST":
+        card = {"question": request.form['question'],
+                "answer": request.form['answer']}
+        db.append(card)
+        save_db()
+        return redirect(url_for("card_view", index=len(db)-1))
+    else:
+        return render_template("add_card.html")
+
         
 @app.route("/api/card/")
 def api_card_list():
--- model.py
+++ model.py
@@ -4,5 +4,10 @@ import json
 def load_db():
     with open("flashcards_db.json", encoding="utf-8") as f: 
         return json.load(f)
-                         
+              
+              
+def save_db():
+    with open("flashcards_db.json", encoding="utf-8", mode='w') as f: 
+        return json.dump(db, f)  
+        
 db = load_db()
